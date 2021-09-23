16e104c6a9d3772c303c0608ac4a2179e9cd3433
Kavya Kushwaha <64400169+kavyaaa2@users.noreply.github.com>
2021-09-23 21:43:33 +0530

    Add files via upload

---
+++ birthday/script.js
@@ -0,0 +1,33 @@
+setTimeout(function(){
+    document.getElementById("p1").innerText="10"
+    setTimeout(function(){
+        document.getElementById("p1").innerText="9"
+        setTimeout(function(){
+            document.getElementById("p1").innerText="8"
+            setTimeout(function(){
+                document.getElementById("p1").innerText="7"
+                setTimeout(function(){
+                    document.getElementById("p1").innerText="6"
+                    setTimeout(function(){
+                        document.getElementById("p1").innerText="5"
+                        setTimeout(function(){
+                            document.getElementById("p1").innerText="4"
+                            setTimeout(function(){
+                                document.getElementById("p1").innerText="3"
+                                setTimeout(function(){
+                                    document.getElementById("p1").innerText="2"
+                                    setTimeout(function(){
+                                        document.getElementById("p1").innerText="1"
+                                        setTimeout(function(){
+                                            document.getElementById("p1").innerText="Happy  Birthday!!"
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
